c058dec4960a631f74ef92501b89e40a974d59e1
Zohaibsajiddeakin <s223192485@deakin.edu.au>
2024-09-17 15:57:31 +0500

    deleted wrong files

--- API/app/middleware/random.py
+++
@@ -1,7 +0,0 @@
-import random
-
-def randompassword():
-    string = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    length = 12
-    newPassword = "".join(random.sample(string,length))
-    return newPassword
